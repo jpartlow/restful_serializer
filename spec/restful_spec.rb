5b6b5fd3bc23089c88a32776c6007b314ab89970
Josh Partlow <jpartlow@glatisant.org>
2011-04-13 17:29:45 -0700

    spec corrections

--- spec/restful_spec.rb
+++ spec/restful_spec.rb
@@ -19,13 +19,13 @@ describe Restful do
 
   describe "looking up model configurations" do
 
-    class Foo; end
-    class FooBar < Foo; end
+    class Super; end
+    class SubClass < Super; end
 
     before(:each) do
       Restful.model_configuration = {
-        :foo => :foo_config,
-        :foo_bar => :foo_bar_config,
+        :super => :super_config,
+        :sub_class => :sub_class_config,
       }
     end
 
@@ -34,36 +34,36 @@ describe Restful do
     end
 
     it "should lookup entries by symbol" do
-      Restful.model_configuration_for(:foo).should == :foo_config
+      Restful.model_configuration_for(:super).should == :super_config
     end
 
     it "should lookup entries from a string" do
-      Restful.model_configuration_for('foo').should == :foo_config
+      Restful.model_configuration_for('super').should == :super_config
     end
 
     it "should lookup entries by Class" do
-      Restful.model_configuration_for(Foo).should == :foo_config
-      Restful.model_configuration_for(FooBar).should == :foo_bar_config
+      Restful.model_configuration_for(Super).should == :super_config
+      Restful.model_configuration_for(SubClass).should == :sub_class_config
     end
 
     it "should lookup entries by instance class" do
-      Restful.model_configuration_for(Foo.new).should == :foo_config
-      Restful.model_configuration_for(FooBar.new).should == :foo_bar_config
+      Restful.model_configuration_for(Super.new).should == :super_config
+      Restful.model_configuration_for(SubClass.new).should == :sub_class_config
     end
 
     describe "with deep structures" do
 
       before(:each) do
         Restful.model_configuration = {
-          :foo => { :setting => {:with => :nested_hash } }
+          :super => { :setting => {:with => :nested_hash } }
         }
       end
 
       it "should provide deep clones of model_configuration elements" do
-        config = Restful.model_configuration_for(:foo)
+        config = Restful.model_configuration_for(:super)
         config[:setting][:with] = "changed"
         Restful.model_configuration.should == {
-          :foo => { :setting => {:with => :nested_hash } }
+          :super => { :setting => {:with => :nested_hash } }
         } 
       end
 
